8e9868296ed51182f92d050da11c537812cfa648
Priyanshu sharma <priyanshushrama709@gmail.com>
2022-02-21 15:21:49 +0530

    Add files via upload

---
+++ precache-manifest.bd88bc1872f92ebdc415ec4be73d80ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aaa01d0905cb957caf7",
+    "url": "/static/css/main.8e5fb45a.chunk.css"
+  },
+  {
+    "revision": "3aaa01d0905cb957caf7",
+    "url": "/static/js/main.1784d742.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5540da1a9dcfb28f526",
+    "url": "/static/css/2.4408960b.chunk.css"
+  },
+  {
+    "revision": "c5540da1a9dcfb28f526",
+    "url": "/static/js/2.4b9ca725.chunk.js"
+  },
+  {
+    "revision": "ed2fc637618963a084c4b8eda6b0b659",
+    "url": "/static/media/bg.ed2fc637.svg"
+  },
+  {
+    "revision": "c28e0536b763204c979078e408527c65",
+    "url": "/index.html"
+  }
+];
